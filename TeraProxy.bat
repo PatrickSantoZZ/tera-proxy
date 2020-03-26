618e395e547aebfbc7bc59b6a3457743d3548014
Pinkie Pie <yarnovax@gmail.com>
2020-03-26 11:14:52 -0400

    Fix typo

--- TeraProxy.bat
+++ TeraProxy.bat
@@ -3,9 +3,9 @@ title TERA Proxy
 cd /d "%~dp0"
 
 if not exist .\settings\_tera-proxy_.json (
-	bin\node\node.exe --use-strict bin\configurator
+	bin\node.exe --use-strict bin\configurator
 	cls
 )
 
-bin\node\node.exe --use-strict --harmony .
+bin\node.exe --use-strict --harmony .
 pause
